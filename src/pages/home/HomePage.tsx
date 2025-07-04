9c9c8a12a0c4039d189be6f9a05c3ee063fad26b
sentares <aydararzybekov2002@mail.ru>
2025-07-04 17:28:58 +0600

    feat: create NoteCard

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,14 +1,7 @@
 import { ThemeToggle } from '@/shared/lib/theme'
-import { Button, Input } from '@mantine/core'
 
 const HomePage = () => {
-	return (
-		<>
-			<Input size='lg' placeholder='Input component' />
-			<Button>Click me</Button>
-			<ThemeToggle />
-		</>
-	)
+	return <>asdsada</>
 }
 
 export default HomePage
