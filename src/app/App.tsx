324c5022bd1e942d3ef7c582d7f0b6709bd73887
sentares <aydararzybekov2002@mail.ru>
2025-07-04 17:23:17 +0600

    feat: update theme config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from './providers/mantine'
 import { Router } from './providers/router'
 import '@mantine/core/styles.css'
+import './styles/index.css'
 
 function App() {
 	return (
