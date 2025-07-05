2dd3eccede4032e39914f99d9c95454d52bf5bdc
Aidar <aydararzybekov2002@mail.ru>
2025-07-05 23:32:49 +0600

    fix: update useFirestore

--- src/shared/hooks/useFirestore.ts
+++ src/shared/hooks/useFirestore.ts
@@ -2,33 +2,54 @@ import {
 	collection,
 	doc,
 	getDocs,
+	getDoc,
 	addDoc,
 	updateDoc,
 	deleteDoc,
-	getDoc,
 	serverTimestamp,
+	type DocumentData,
 } from 'firebase/firestore'
 import { db } from '@/shared/lib/firebase'
 
 export const apiPaths = {
 	notes: 'notes',
+	profiles: 'profiles',
 } as const
 
 export const useFirestore = () => {
-	const getAll = async (path: string) => {
+	const getAll = async <T = DocumentData>(path: string): Promise<T[]> => {
 		const collectionRef = collection(db, path)
-		const response = await getDocs(collectionRef)
-		return response.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+		const snapshot = await getDocs(collectionRef)
+		return snapshot.docs.map(
+			doc =>
+				({
+					id: doc.id,
+					...doc.data(),
+				} as T)
+		)
 	}
 
-	const getOne = async (path: string, id: string) => {
+	const getOne = async <T = DocumentData>(
+		path: string,
+		id: string
+	): Promise<T> => {
 		const documentRef = doc(db, path, id)
-		const response = await getDoc(documentRef)
-		if (response.exists()) return { id: response.id, ...response.data() }
-		else throw new Error('Document not found')
+		const snapshot = await getDoc(documentRef)
+
+		if (!snapshot.exists()) {
+			throw new Error('Document not found')
+		}
+
+		return {
+			id: snapshot.id,
+			...snapshot.data(),
+		} as T
 	}
 
-	const add = async (path: string, data: { title: string }) => {
+	const add = async <T = DocumentData>(
+		path: string,
+		data: Partial<T>
+	): Promise<string> => {
 		const collectionRef = collection(db, path)
 		const documentRef = await addDoc(collectionRef, {
 			...data,
@@ -37,12 +58,16 @@ export const useFirestore = () => {
 		return documentRef.id
 	}
 
-	const update = async (path: string, id: string, data: { title: string }) => {
+	const update = async <T = DocumentData>(
+		path: string,
+		id: string,
+		data: Partial<T>
+	): Promise<void> => {
 		const documentRef = doc(db, path, id)
 		await updateDoc(documentRef, data)
 	}
 
-	const remove = async (path: string, id: string) => {
+	const remove = async (path: string, id: string): Promise<void> => {
 		const documentRef = doc(db, path, id)
 		await deleteDoc(documentRef)
 	}
